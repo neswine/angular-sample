185e9cd1a7c7e818193d82df9403d1ab24a14364
Neswine <neswine@Neswines-MacBook-Air.local>
2022-02-12 15:37:43 +0530

    api url added and modified api service

--- src/app/shared/services/http-base-service.service.ts
+++ src/app/shared/services/http-base-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -15,12 +16,12 @@ export class HttpBaseServiceService {
    * @params {string} url
    * @params {HttpParams} urlParams
    */
-  get(url: string, urlParams?: HttpParams): Observable<any> {
-    return this.http.get('http://localhost' + url, { params: urlParams});
+  public get(url: string, urlParams?: HttpParams): Observable<any> {
+    return this.http.get(`${environment.apiUrl}/${url}`, { params: urlParams});
   }
 
   public post(url: string,body: any): Observable<any> {
-    return this.http.post('http://localhost' + url, body, {});
+    return this.http.post(`${environment.apiUrl}/${url}`, body, {});
   }
   
 }
