ef12f2fbc5f53ae019fe167543cb786e6d934b33
Neswine <neswine@Neswines-MacBook-Air.local>
2022-02-12 15:09:12 +0530

    initail commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { LibraryListPageComponent } from './library/pages/library-list-page/library-list-page.component';
+import { ProfilePageComponent } from './profile/pages/profile-page/profile-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'library', 
+    children: [{
+      path: 'list',
+      component: LibraryListPageComponent
+    }],
+  },
+  { 
+    path: 'profile', 
+    children: [{
+      path: '',
+      component: ProfilePageComponent
+    }],
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LibraryModule } from './library/library.module';
+import { SharedModule } from './shared/shared.module';
+import { ProfileModule } from './profile/profile.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LibraryModule,
+    ProfileModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
